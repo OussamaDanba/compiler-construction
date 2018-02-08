c4f1a47bd484fd2437d4e206538c711aaf85dde1
Shadoxfix <OussamaDanba@gmail.com>
2018-02-08 17:04:07 +0100

    Add support for reading files

--- slp-compiler/src/main.rs
+++ slp-compiler/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+	// Read in specified file into a String. We don't want to panick here so we use exit. 
+	let args: Vec<String> = env::args().collect();
+	let filename: &str = args.get(1).unwrap_or_else(|| {
+		println!("No filename specified.");
+		::std::process::exit(1)
+	});
+
+	let mut file: File = File::open(filename).unwrap_or_else(|e| {
+		println!("{}", e);
+		::std::process::exit(1)
+	});
+
+	let mut file_contents = String::new();
+	file.read_to_string(&mut file_contents).unwrap_or_else(|e| {
+		println!("{}", e);
+		::std::process::exit(1)
+	});
+
+	println!("{}", file_contents);
 }

e9c783ca28b4c55b7a40afd8702dca5387b91f37
Shadoxfix <OussamaDanba@gmail.com>
2018-03-01 18:21:21 +0100

    Define AST and implement part of expression parsing

--- spl-compiler/src/parser/ast.rs
+++ spl-compiler/src/parser/ast.rs
@@ -1,9 +1,94 @@
+pub type Ident = String;
+
+#[derive(Debug)]
+pub struct SPL {
+	vars: Vec<Variable>,
+	funs: Vec<Function>
+}
+
+#[derive(Debug)]
+pub struct Variable {
+	name: Ident,
+	vtype: Option<Type>,
+	value: Expression
+}
+
+#[derive(Debug)]
+pub struct Function {
+	name: Ident,
+	args: Vec<Ident>,
+	ftype: Option<Type>,
+	vars: Vec<Variable>,
+	stmts: Vec<Statement>
+}
+
 #[derive(Debug)]
 pub enum Type {
 	TInt,
 	TBool,
 	TChar,
+	TVoid,
 	TTuple(Box<Type>, Box<Type>),
 	TList(Box<Type>),
-	TIdent(String)
+	TArrow(Vec<Type>, Box<Type>),
+	TIdent(Ident)
+}
+
+#[derive(Debug)]
+pub enum Statement {
+	If(Expression, Vec<Statement>, Vec<Statement>),
+	While(Expression, Vec<Statement>),
+	Assignment(Ident, Vec<Field>, Expression),
+	FunCall(Expression), // To prevent code duplication we simply treat this as an expression
+	Return(Option<Expression>)
+}
+
+#[derive(Debug)]
+pub enum Expression {
+	Ident(Ident, Vec<Field>),
+	Op2(Box<Expression>, Op2, Box<Expression>),
+	Op1(Op1, Box<Expression>),
+	Lit(Literal),
+	FunCall(Ident, Vec<Expression>),
+	Tuple(Box<Expression>, Box<Expression>)
+}
+
+#[derive(Debug)]
+pub enum Field {
+	Head,
+	Tail,
+	First,
+	Second
+}
+
+#[derive(Debug)]
+pub enum Op2 {
+	Addition,
+	Subtraction,
+	Multiplication,
+	Division,
+	Modulo,
+	Equals,
+	LessThan,
+	GreaterThan,
+	LessEquals,
+	GreaterEquals,
+	NotEquals,
+	And,
+	Or,
+	Cons
+}
+
+#[derive(Debug)]
+pub enum Op1 {
+	Not,
+	Negation
+}
+
+#[derive(Debug)]
+pub enum Literal {
+	Int(i64),
+	Char(char),
+	Bool(bool),
+	EmptyList
 }
--- spl-compiler/src/parser/mod.rs
+++ spl-compiler/src/parser/mod.rs
@@ -2,49 +2,109 @@ pub use self::ast::*;
 mod ast;
 
 use scanner::Token;
-use combine::{satisfy, token, many, Stream, Parser};
+use combine::{satisfy, satisfy_map, token, many, optional, Stream, Parser};
+
+// parser!{
+// 	fn parse_type[I]()(I) -> Type
+// 	  where [I: Stream<Item=Token>]
+// 	{
+// 		let tuple_parser = (token(Token::TokenParenOpen),
+// 			parse_type(),
+// 			token(Token::TokenComma),
+// 			parse_type(),
+// 			token(Token::TokenParenClose))
+// 			.map(|t| Type::TTuple(Box::new(t.1), Box::new(t.3)));
+
+// 		let list_parser = (token(Token::TokenBracketOpen),
+// 			parse_type(),
+// 			token(Token::TokenBracketClose))
+// 			.map(|t| Type::TList(Box::new(t.1)));
+
+// 		choice!(
+// 			token(Token::TokenIntKeyword).map(|_| Type::TInt),
+// 			token(Token::TokenBoolKeyword).map(|_| Type::TBool),
+// 			token(Token::TokenCharKeyword).map(|_| Type::TChar),
+// 			tuple_parser,
+// 			list_parser,
+// 			parse_identifier()
+// 		)
+// 	}
+// }
 
 parser!{
-	fn parse_type[I]()(I) -> Type
+	fn parse_identifier[I]()(I) -> Ident
 	  where [I: Stream<Item=Token>]
 	{
-		let tuple_parser = (token(Token::TokenParenOpen),
-			parse_type(),
-			token(Token::TokenComma),
-			parse_type(),
-			token(Token::TokenParenClose))
-			.map(|t| Type::TTuple(Box::new(t.1), Box::new(t.3)));
+		satisfy_map(|x: Token| match x {
+			Token::TokenIdentifier(ident) => Some(ident),
+			_ => None,
+		})
+	}
+}
 
-		let list_parser = (token(Token::TokenBracketOpen),
-			parse_type(),
-			token(Token::TokenBracketClose))
-			.map(|t| Type::TList(Box::new(t.1)));
+parser!{
+	fn parse_field[I]()(I) -> Vec<Field>
+	  where [I: Stream<Item=Token>]
+	{
+		many((token(Token::TokenDot), satisfy_map(|x: Token| match x {
+			Token::TokenIdentifier(ident) => match ident.as_ref() {
+				"hd" => Some(Field::Head),
+				"tl" => Some(Field::Tail),
+				"fst" => Some(Field::First),
+				"snd" => Some(Field::Second),
+				_ => None,
+			},
+			_ => None,
+		})).map(|t| t.1))
+	}
+}
+
+parser!{
+	fn parse_literal[I]()(I) -> Literal
+	  where [I: Stream<Item=Token>]
+	{
+		let parse_int = (optional(token(Token::TokenMinus)), satisfy_map(|x| match x {
+			Token::TokenInt(i) => Some(i),
+			_ => None
+		})).map(|(min, num)| Literal::Int( match min {
+			Some(_) => num.wrapping_neg(),
+			_ => num
+		}));
+
+		let parse_char = satisfy_map(|x| match x {
+			Token::TokenChar(c) => Some(c),
+			_ => None
+		}).map(|c| Literal::Char(c));
+
+		let parse_bool = satisfy_map(|x| match x {
+			Token::TokenBool(b) => Some(b),
+			_ => None
+		}).map(|b| Literal::Bool(b));
+
+		let parse_empty_list = (token(Token::TokenBracketOpen), token(Token::TokenBracketClose))
+			.map(|_| Literal::EmptyList);
 
 		choice!(
-			token(Token::TokenIntKeyword).map(|_| Type::TInt),
-			token(Token::TokenBoolKeyword).map(|_| Type::TBool),
-			token(Token::TokenCharKeyword).map(|_| Type::TChar),
-			tuple_parser,
-			list_parser,
-			parse_identifier()
+			parse_int,
+			parse_char,
+			parse_bool,
+			parse_empty_list
 		)
 	}
 }
 
 parser!{
-	fn parse_identifier[I]()(I) -> Type
+	fn parse_expression_no_op2[I]()(I) -> Expression
 	  where [I: Stream<Item=Token>]
 	{
-		satisfy(|x: Token| match x {
-			Token::TokenIdentifier(_) => true,
-			_ => false
-		}).map(|x: Token| match x {
-			Token::TokenIdentifier(ident) => Type::TIdent(ident),
-			_ => panic!("Unreachable")
-		})
+		choice!(
+			(parse_identifier(), parse_field()).map(|(i, fs)| Expression::Ident(i, fs)),
+			parse_literal().map(|lit| Expression::Lit(lit)),
+			token(Token::TokenIntKeyword).map(|_| Expression::Lit(Literal::EmptyList))
+		)
 	}
 }
 
 pub fn parse(tokens: &[Token]) {
-	println!("{:?}", many::<Vec<Type>, _>(parse_type()).parse(tokens));
+	println!("{:?}", parse_expression_no_op2().parse(tokens));
 }

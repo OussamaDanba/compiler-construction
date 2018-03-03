1f7a4f280719446fb617ecb393dd8bac56c01357
Shadoxfix <OussamaDanba@gmail.com>
2018-03-03 18:29:15 +0100

    Partially implement pretty printer

--- spl-compiler/src/parser/ast.rs
+++ spl-compiler/src/parser/ast.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub type Ident = String;
 
 #[derive(Debug)]
@@ -91,4 +93,104 @@ pub enum Literal {
 	Char(char),
 	Bool(bool),
 	EmptyList
+}
+
+fn indent(input: String) -> String {
+	input.lines().map(|x| format!("\t{}\n", x)).collect()
+}
+
+impl fmt::Display for Statement {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", match *self {
+			Statement::If(ref exp, ref if_stmts, ref else_stmts) =>
+				format!("if({}) {{\n{}}}{}", exp, indent(if_stmts.iter()
+					.map(|x| format!("{}\n", x)).collect::<Vec<String>>().concat()),
+						if else_stmts.is_empty() {
+							String::new()
+						} else {
+							format!(" else {{\n{}}}", indent(else_stmts.iter().map(|x| format!("{}\n", x)).collect::<Vec<String>>().concat()))
+						}
+				),
+			Statement::While(ref exp, ref stmts) => format!("while({}) {{\n{}}}", exp,
+				indent(stmts.iter().map(|x| format!("{}\n", x)).collect::<Vec<String>>().concat())),
+			Statement::Assignment(ref ident, ref fields, ref exp) => format!("{}{} = {}", ident,
+				fields.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().concat(), exp),
+			Statement::FunCall(ref exp) => format!("{};", exp),
+			Statement::Return(ref exp) => match *exp {
+				Some(ref x) => format!("return {};", x),
+				_ => String::from("return;")
+			}
+		})
+	}
+}
+
+impl fmt::Display for Expression {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", match *self {
+			Expression::Ident(ref ident, ref fields) =>	format!("{}{}", ident,
+				fields.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().concat()),
+			Expression::Op2(_, _, _) => String::new(), //TODO: Implement with correct parenthesis
+			Expression::Op1(ref op1, ref exp) => format!("{}{}", op1, exp),
+			Expression::Lit(ref lit) => format!("{}", lit),
+			Expression::FunCall(ref ident, ref exps) => format!("{}({})", ident,
+				exps.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(",")),
+			Expression::Tuple(ref l, ref r) => format!("({}, {})", l, r)
+		})
+	}
+}
+
+impl fmt::Display for Field {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", match *self {
+			Field::Head => String::from(".hd"),
+			Field::Tail => String::from(".tl"),
+			Field::First => String::from(".fst"),
+			Field::Second => String::from(".snd")
+		})
+	}
+}
+
+impl fmt::Display for Op2 {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", match *self {
+			Op2::Addition => String::from("+"),
+			Op2::Subtraction => String::from("-"),
+			Op2::Multiplication => String::from("*"),
+			Op2::Division => String::from("/"),
+			Op2::Modulo => String::from("%"),
+			Op2::Equals => String::from("=="),
+			Op2::LessThan => String::from("<"),
+			Op2::GreaterThan => String::from(">"),
+			Op2::LessEquals => String::from("<="),
+			Op2::GreaterEquals => String::from(">="),
+			Op2::NotEquals => String::from("!="),
+			Op2::And => String::from("&&"),
+			Op2::Or => String::from("||"),
+			Op2::Cons => String::from(":")
+		})
+	}
+}
+
+impl fmt::Display for Op1 {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", match *self {
+			Op1::Not => String::from("!"),
+			Op1::Negation => String::from("-")
+		})
+	}
+}
+
+impl fmt::Display for Literal {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", match *self {
+			Literal::Int(i) => i.to_string(),
+			Literal::Char(c) => format!("'{}'", c.to_string()),
+			Literal::Bool(b) => if b {
+				String::from("True")
+			} else {
+				String::from("False")
+			},
+			Literal::EmptyList => String::from("[]")
+		})
+	}
 }
